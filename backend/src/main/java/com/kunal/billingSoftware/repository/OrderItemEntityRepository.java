a3587d2912715747813fab4989092f1048eb7a4b
kunalpawar04 <pawarkunal3996@gmail.com>
2025-10-19 17:06:21 +0530

    Add unit tests for OrderEntityRepository, CategoryRepository, ItemRepository & UserRepository

--- backend/src/main/java/com/kunal/billingSoftware/repository/OrderItemEntityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kunal.billingSoftware.repository;
-
-import com.kunal.billingSoftware.entity.OrderItemEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemEntityRepository extends JpaRepository<OrderItemEntity, Long> {
-
-}
